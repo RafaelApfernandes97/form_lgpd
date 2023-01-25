c8ddf4ae325522b7a0f48f48b924ff3baee902d5
RafaelApfernandes97 <109108455+RafaelApfernandes97@users.noreply.github.com>
2023-01-25 16:26:33 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+var expanded = false;
+
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
+
+var expanded = false;
+
+function showCheckboxes2() {
+  var checkboxes = document.getElementById("checkboxes2");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
